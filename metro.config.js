4b375c9ec2cfcb07e13272300edfb3048c3b36a2
guohanlin <guohanlin@xyz.cn>
2020-07-21 15:58:23 +0800

    [新增功能](master): ReactNativeLockTable项目初始化

---
+++ metro.config.js
@@ -0,0 +1,23 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+const path = require('path');
+
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+  },
+  resolver: {
+    sourceExts: ['jsx', 'js', 'ts', 'tsx'],
+    providesModuleNodeModules: ['react-native', 'react'],
+  },
+  projectRoot: path.resolve(__dirname),
+};
---
+++ src/LockTableView.tsx
@@ -0,0 +1,16 @@
+import {StyleSheet, View} from 'react-native';
+
+interface Props {}
+
+/**
+ * 注释: 双向锁定表格
+ * 时间: 2020/7/21 0021 14:06
+ * @author 郭翰林
+ * @param props
+ * @constructor
+ */
+export default function LockTableView(props: Props) {
+  return <View style={{flex: 1}} />;
+}
+
+const styles = StyleSheet.create({});
