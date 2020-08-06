2eb6c7f7bc93cf1c24f1c6f47a8e4d6a3257b7b7
guohanlin <guohanlin@xyz.cn>
2020-08-06 10:22:56 +0800

    [代码完善](master): LockTableView表格功能完善

--- src/LockTableView.tsx
+++ src/LockTableView.tsx
@@ -1,11 +1,7 @@
 import {StyleSheet, View, Text, Platform, FlatList, ScrollView} from 'react-native';
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 
 interface Props {
-  //是否锁定列
-  lockColumn?: boolean;
-  //是否锁定行
-  lockRow?: boolean;
   //标题数据
   titleData: string[];
   //数据源
@@ -14,10 +10,14 @@ interface Props {
   textSize?: number;
   //单元格文字颜色
   textColor?: string;
-  //单元格内边距
-  cellPadding?: number;
+  //单元格最大宽度
+  cellMaxWidth?: number;
+  //单元格高度
+  cellHeight?: number;
   //第一行背景色
   firstRowBackGroundColor?: string;
+  //第一列背景色
+  firstColumnBackGroundColor?: string;
   //表头字体颜色
   tableHeadTextColor?: string;
 }
@@ -32,18 +32,34 @@ interface Props {
 export default function LockTableView(props: Props) {
   const border_width = Platform.OS === 'ios' ? StyleSheet.hairlineWidth : StyleSheet.hairlineWidth * 2;
   let columnMaxWidth: number[] = [];
-  //计算每列最大宽度
+  let firstColumnData = [];
+
+  //计算每列最大宽度、分割数据
+  let scale = props.textSize;
+  props.titleData.map((value, i) => {
+    if (value.length * scale < props.cellMaxWidth) {
+      columnMaxWidth[i] = value.length * scale;
+    } else {
+      columnMaxWidth[i] = props.cellMaxWidth;
+    }
+  });
   props.tableData.map((item, i) => {
     Object.values(item).map((value, j) => {
-      if (i === 0) {
-        columnMaxWidth[j] = value.length * 15;
-      } else {
-        if (columnMaxWidth[j] < value.length * 15) {
-          columnMaxWidth[j] = value.length * 15;
+      if (j == 0) {
+        firstColumnData.push(value);
+      }
+      if (columnMaxWidth[j] < value.length * scale) {
+        if (value.length * scale < props.cellMaxWidth) {
+          columnMaxWidth[j] = value.length * scale;
+        } else {
+          columnMaxWidth[j] = props.cellMaxWidth;
         }
       }
     });
+    //删除对象第一个属性数据
+    delete item[Object.keys(item)[0]];
   });
+
   /**
    * 注释: 绘制每行数据
    * 时间: 2020/7/23 0023 9:14
@@ -55,9 +71,12 @@ export default function LockTableView(props: Props) {
       childrenViews.push(
         <Text
           style={{
-            fontSize: 14,
-            color: '#333',
-            width: columnMaxWidth[i],
+            fontSize: props.textSize,
+            color: props.textColor,
+            width: columnMaxWidth[i + 1],
+            height: props.cellHeight,
+            textAlign: 'center',
+            textAlignVertical: 'center',
             borderWidth: border_width,
             borderColor: '#e7e7e7',
           }}>
@@ -68,20 +87,171 @@ export default function LockTableView(props: Props) {
     return <View style={{flexDirection: 'row', alignItems: 'center'}}>{childrenViews}</View>;
   }
 
-  return (
-    <View style={{flex: 1}}>
-      <ScrollView horizontal={true} removeClippedSubviews={true} keyboardShouldPersistTaps={'handled'}>
-        <FlatList
-          data={props.tableData}
+  /**
+   * 注释: 绘制第一行行数据
+   * 时间: 2020/8/5 0005 17:33
+   * @author 郭翰林
+   * @param rowData
+   * @returns {any}
+   */
+  function renderFirstRowCell(rowData: string[]) {
+    let childrenViews = [];
+    rowData.map((item, i) => {
+      childrenViews.push(
+        <Text
+          style={{
+            fontSize: props.textSize,
+            color: props.textColor,
+            width: columnMaxWidth[i + 1],
+            height: props.cellHeight,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+            borderWidth: border_width,
+            borderColor: '#e7e7e7',
+          }}>
+          {item}
+        </Text>,
+      );
+    });
+    return <View style={{flexDirection: 'row', alignItems: 'center'}}>{childrenViews}</View>;
+  }
+
+  /**
+   * 注释: 绘制表格头
+   * 时间: 2020/8/5 0005 17:36
+   * @author 郭翰林
+   * @returns {any}
+   */
+  function renderHeaderView() {
+    let first = props.titleData.shift();
+    return (
+      <View style={{flexDirection: 'row', backgroundColor: props.firstRowBackGroundColor}}>
+        <View
+          style={{
+            width: columnMaxWidth[0] + props.textSize,
+            height: props.cellHeight,
+            borderWidth: border_width,
+            borderColor: '#e7e7e7',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: props.textSize,
+              color: props.textColor,
+              textAlign: 'center',
+              textAlignVertical: 'center',
+            }}>
+            {first}
+          </Text>
+        </View>
+        <ScrollView
+          ref={headScrollView}
+          style={{borderRightWidth: border_width, borderColor: '#e7e7e7'}}
+          horizontal={true}
+          removeClippedSubviews={true}
           showsHorizontalScrollIndicator={false}
           showsVerticalScrollIndicator={false}
-          renderItem={rowData => {
-            return renderRowCell(rowData.item, rowData.index);
+          bounces={false}
+          scrollEnabled={false}
+          keyboardShouldPersistTaps={'handled'}>
+          {renderFirstRowCell(props.titleData)}
+        </ScrollView>
+      </View>
+    );
+  }
+
+  /**
+   * 注释: 绘制第一列锁定数据
+   * 时间: 2020/8/5 0005 15:21
+   * @author 郭翰林
+   * @param rowData
+   * @param index
+   * @returns {any}
+   */
+  function renderFirstCell(rowData: object, index: number) {
+    return (
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+          borderColor: '#e7e7e7',
+          height: props.cellHeight,
+          borderWidth: border_width,
+        }}>
+        <Text
+          style={{
+            fontSize: props.textSize,
+            color: props.textColor,
+            textAlign: 'center',
+            textAlignVertical: 'center',
+          }}>
+          {rowData}
+        </Text>
+      </View>
+    );
+  }
+
+  let lockList = useRef(null);
+  let headScrollView = useRef(null);
+
+  return (
+    <View style={{flex: 1}}>
+      {renderHeaderView()}
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        {/*锁定列*/}
+        <View style={{width: columnMaxWidth[0] + props.textSize}}>
+          <FlatList
+            ref={lockList}
+            contentContainerStyle={{
+              backgroundColor: props.firstColumnBackGroundColor,
+            }}
+            scrollEnabled={false}
+            data={firstColumnData}
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+            renderItem={rowData => {
+              return renderFirstCell(rowData.item, rowData.index);
+            }}
+          />
+        </View>
+        {/*滚动数据*/}
+        <ScrollView
+          style={{borderRightWidth: border_width, borderColor: '#e7e7e7'}}
+          horizontal={true}
+          bounces={false}
+          onScroll={event => {
+            headScrollView.current.scrollTo({x: event.nativeEvent.contentOffset.x});
           }}
-        />
-      </ScrollView>
+          keyboardShouldPersistTaps={'handled'}>
+          <FlatList
+            data={props.tableData}
+            showsHorizontalScrollIndicator={false}
+            showsVerticalScrollIndicator={false}
+            onScroll={event => {
+              lockList.current.scrollToOffset({
+                animated: false,
+                offset: event.nativeEvent.contentOffset.y,
+              });
+            }}
+            renderItem={rowData => {
+              return renderRowCell(rowData.item, rowData.index);
+            }}
+          />
+        </ScrollView>
+      </View>
     </View>
   );
 }
+
+LockTableView.defaultProps = {
+  textSize: 15,
+  textColor: '#666',
+  tableHeadTextColor: '#666',
+  cellMaxWidth: 150,
+  cellHeight: 35,
+  firstRowBackGroundColor: '#F0F9FF',
+  firstColumnBackGroundColor: '#FFF9F7',
+};
 
 const styles = StyleSheet.create({});
